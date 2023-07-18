737b0cdb8690d8145a76520fbf1c5281feedd28f
Atta Khan <35913737+Attaullahkhanit@users.noreply.github.com>
2023-07-19 00:17:06 +0500

    my-commit

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../Components/Navbar/Navbar'
+
+function Home() {
+  return (
+    <>
+        <Navbar />
+        <button className='btn btn-primary'>Click Me</button>
+    </>
+  )
+}
+
+export default Home
